eedf89fb2263e7560638fdf711374b71f89f79df
varun0911 <varunsai23076@gmail.com>
2024-06-12 12:26:24 +0530

    added js for behaviour

--- index.js
+++ index.js
@@ -20,4 +20,59 @@ const taskModal = document.querySelector(".task__modal__body");
 console.log(taskContents);
 console.log(taskModal);
 
-const htmlTaskContent = ({ id, title, description, type, url}) => {};
+const htmlTaskContent = ({ id, title, description, type, url}) => `
+    <div class="col-md-6 col-lg-4 mt-3" id=${id}>
+        <div class="card shadow task__card">
+            <div class="card-header d-flex justify-content-end task__card__header">
+                <button type="button" class="btn btn-outline-info" name=${id}>
+                <i class="fas fa-pencil-alt" name=${id}></i>
+                </button>
+                <button type="button" class="btn btn-outline-danger" name=${id}>
+                <i class="fas fa-trash-alt" name=${id}></i>
+                </button>
+            </div>
+            <div class="card-body">
+                ${
+                    url &&
+                    `<img width="100%" src=${url} alt="card image" class="card-img-top md-3 rounded=lg" />`
+                }
+                <h4 class="card-title task__card__title">${title}</h4>
+                <p class="description trim-3-lines text-muted">${description}</p>
+                <div class="tags text-white d-flex flex-wrap">
+                    <span class="badge bg-primary">${type}</span>
+                </div>
+            </div>
+            <div class="card-footer">
+                <button type="button" class="btn btn-outline-primary float-right" data-bs-toggle="modal" data-bs-target="#showTask">Open Task</button>
+            </div>
+        </div>
+    </div>
+`;
+
+// modal body when clicked open
+
+const htmlModalContent = ({ id, title, type, description, url}) => {
+    const date = new Date(parseInt(id));
+    return `
+    <div id=${id}>
+        ${
+            url && 
+            `<img width="100%" src=${url} alt="card image" class="img-fluid place__holder__img mb-3" />`
+        }
+        <strong class="text-muted text-sm">Created on: ${date.toDateString()}</strong>
+        <h2 class="my-3">${title}</h2>
+        <p class="text-muted">${description}</p>
+    </div>    
+    `
+};
+
+const updateLocalStorage = () => {
+    localStorage.setItem(
+        "tasky",
+        JSON.stringify({
+            tasks: state.taskList,
+        })
+    );
+};
+
+//load initial data
